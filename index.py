8d88de2aaa6514325bcc0a64559d4263d4d7af6b
Elbek Khakimbekov <hakimbekovelbek@gmail.com>
2022-09-08 20:17:25 +0500

    Add theory about ssh keys

---
+++ index.py
@@ -0,0 +1,13 @@
+# ssh-keygen -t rsa -b 4096 -C "your@email.com"
+
+# ssh key generator
+# -t = type
+# -b = bits
+
+# ssh -T git@github.com
+# -T = tunnel connection to git@github.com
+
+# ! Public key = замочная скважина
+# ? Private key = ключ к замку
+
+
